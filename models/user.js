7a60ef28912ec7ad1dabfe5c82b1109f40c9c9a6
kubat <kubat.aiupov@gmail.com>
2023-12-11 14:01:35 +0100

    init user schema

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema();
+
+
+const User = new userSchema({
+  userName: {
+    type: String,
+    required: [true, 'Username must be provided'],
+    unique: [true, 'Username must be unique']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email must be provided'],
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: (str) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str),
+      message: '{VALUE} is not valid email!'
+    }
+  },
+  password: {
+    type: String,
+    required: [true, 'Provide password'],
+    trim: true,
+  }
+})
+
+module.exports = mongoose.model('User', userSchema);
